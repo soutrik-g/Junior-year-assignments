6af0067707c6ccaca03f4f239542b8444b139f09
soutrik-g <soutrik.guhathakurta@gmail.com>
2023-10-17 02:29:41 -0400

    Lab 1 Operating systems

---
+++ Operating_Systems/L1_P1/jiffies.c
@@ -0,0 +1,60 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#include <linux/jiffies.h>
+
+/* Meta Information */
+
+
+#define BUFFER_SIZE 128
+#define PROC_NAME "jiffies"
+
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+    .owner = THIS_MODULE,
+    .read = proc_read,
+};
+/* This function is called when the module is loaded. */
+static int proc_init(void) {
+        /* creates the /proc/jiffies entry */
+        proc_create(PROC_NAME, 0666, NULL, &proc_ops);
+        return 0;
+}
+
+/* This function is called when the module is removed. */
+static void proc_exit (void) {
+
+        /* removes the /proc/jiffies entry */
+        remove_proc_entry(PROC_NAME, NULL);
+}
+
+/* This function is called each time /proc/jiffies is read */
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+int rv = 0;
+char buffer[BUFFER_SIZE];
+static int completed = 0;
+
+if (completed) {
+completed = 0;
+return 0;
+}
+
+completed = 1;
+/* print jiffies */
+rv = sprintf(buffer, "JIFFIES: %lu\n",jiffies);
+/* copies kernel space buffer to user space usr buf */
+copy_to_user(usr_buf, buffer, rv);
+return rv;
+}
+
+/* Macros for registering module entry and exit points. */
+module_init(proc_init);
+module_exit(proc_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Soutrik");
+MODULE_DESCRIPTION("Assignment_1 part_1");
